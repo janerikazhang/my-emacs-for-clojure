2d449d11ff203aae758306abf0a3d2d13b679e89
Rika <janerikazhang@gmail.com>
2016-05-10 22:40:03 +0800

    add everything

---
+++ personal/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm--remap-mouse-mode t)
+ '(helm-always-two-windows t)
+ '(helm-autoresize-mode nil)
+ '(helm-input-method-verbose-flag nil)
+ '(helm-move-to-line-cycle-in-source nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
